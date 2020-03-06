be01b86cdac28085bfd184e0812bc654ba081784
Blake Wang <bwang@intouchhealth.com>
2020-03-06 15:25:52 -0800

    Choose random centroids

--- k_means.py
+++ k_means.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python3
 
+# https://jakevdp.github.io/PythonDataScienceHandbook/05.11-k-means.html
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets.samples_generator import make_blobs
+from scipy.spatial import distance
 
-# https://jakevdp.github.io/PythonDataScienceHandbook/05.11-k-means.html
+class KMeans:
+    def __init__(self, data, num_clusters, random_state):
+        self.data = data
+        self.num_clusters = num_clusters
+        self.random_state = random_state
 
-# I'm cheating here because I know the number of clusters ahead of time.
+    def run(self):
+#        distance.euclidean([1, 0, 0], [0, 1, 0])
+
+        # Initially choose centroids to be K random points from the data.
+        self.__choose_random_centroids()
+        print(self.centroids)
+
+        # Loop until the centroids don't change.
+        #   Assign each data point to a cluster by min distance to centroid.
+        #   Recalculate the centroid of each cluster.
+
+    def __choose_random_centroids(self):
+        np.random.seed(self.random_state)
+        indices = np.random.choice(
+            len(self.data),
+            size=self.num_clusters,
+            replace=False
+        )
+        self.centroids = self.data[indices]
 
 NUM_CLUSTERS = 3
 CLUSTER_STD = 0.8
@@ -22,14 +47,5 @@ X, z = make_blobs(
 #plt.scatter(X[:, 0], X[:, 1], s=50)
 #plt.show()
 
-class KMeans:
-    def __init__(self, num_clusters, cluster_std=0.5):
-        self.num_clusters = num_clusters
-        self.cluster_std = cluster_std
-
-    def dump(self):
-        print('num_clusters', self.num_clusters)
-        print('cluster_std', self.cluster_std)
-
-clf = KMeans(num_clusters=20, cluster_std=50)
-clf.dump()
+clf = KMeans(data=X, num_clusters=NUM_CLUSTERS, random_state=RANDOM_STATE)
+clf.run()
