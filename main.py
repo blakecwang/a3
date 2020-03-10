59becbdc7a9b54aae09cd79b67179d9c236403b2
Blake Wang <bwang@intouchhealth.com>
2020-03-10 09:08:18 -0700

    Get expected sillouette score values

--- main.py
+++ main.py
@@ -8,24 +8,25 @@ from classes.my_k_means import MyKMeans
 from classes.my_expect_max import MyExpectMax
 
 X, y = make_blobs(
-    centers=3,
+    centers=5,
     n_features=10,
     n_samples=1000,
     random_state=11
 )
 
-for n_clusters in range(4,5):
+for n_clusters in range(3, 8):
     clusterers = [
         MyKMeans(data=X, n_clusters=n_clusters),
-#        MyExpectMax(data=X, n_clusters=n_clusters)
+        MyExpectMax(data=X, n_clusters=n_clusters)
     ]
 
     for clusterer in clusterers:
-        print(n_clusters, clusterer.__class__.__name__)
         cluster_labels, centers, iters = clusterer.run()
-#        score = silhouette_score(X, cluster_labels)
-#        print(f"  {score}")
-#        print('')
+        score = silhouette_score(X, cluster_labels)
+
+        print(n_clusters, clusterer.__class__.__name__)
+        print(f"  {score}")
+        print('')
 
 #        print('cluster_labels', np.unique(cluster_labels, return_counts=True))
 #        print('centers', centers)
