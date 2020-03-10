87f9fa9a1607c4269e202c82e64defd6bc90a060
Blake Wang <bwang@intouchhealth.com>
2020-03-10 07:53:30 -0700

    Update MyKMeans to handle n dimensional data

---
+++ classes/my_k_means.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# https://jakevdp.github.io/PythonDataScienceHandbook/05.11-k-means.html
+
+import numpy as np
+from scipy.spatial import distance
+
+class MyKMeans:
+    def __init__(self, data, n_clusters, random_state=11, max_iters=100):
+        self.data = data
+        self.n_clusters = n_clusters
+        self.random_state = random_state
+        self.max_iters = max_iters
+        self.cluster_labels = np.zeros(data.shape[0], dtype=int)
+        self.prev_centers = np.zeros((n_clusters, data.shape[1]))
+        self.iters = 0
+
+    def run(self):
+        self.__choose_random_centers()
+        while not np.array_equal(self.centers, self.prev_centers) and self.iters < self.max_iters:
+            self.__assign_points_to_clusters()
+            self.__find_new_centers()
+            self.iters += 1
+        return self.cluster_labels, self.centers, self.iters
+
+    def __choose_random_centers(self):
+        np.random.seed(self.random_state)
+        indices = np.random.choice(
+            len(self.data),
+            size=self.n_clusters,
+            replace=False
+        )
+        self.centers = self.data[indices]
+
+    def __assign_points_to_clusters(self):
+        for i, point in enumerate(self.data):
+            shortest_distance = np.inf
+            for j, center in enumerate(self.centers):
+                d = distance.euclidean(point, center)
+                if d < shortest_distance:
+                    self.cluster_labels[i] = j
+                    shortest_distance = d
+
+    def __find_new_centers(self):
+        self.prev_centers = np.copy(self.centers)
+        for i in range(self.n_clusters):
+            mask = np.array([label == i for label in self.cluster_labels])
+            self.centers[i] = np.mean(self.data[mask], axis=0)
--- main.py
+++ main.py
@@ -3,40 +3,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets.samples_generator import make_blobs
-from classes.k_means import KMeans
+from classes.my_k_means import MyKMeans
 from classes.expect_max import ExpectMax
 from classes.helpers import Helpers
 
 # These are used both for generating data and for calculating clusters (kind of
 # cheating).
-NUM_CLUSTERS = 3
+N_CLUSTERS = 5
 CLUSTER_STD = 1
 
-X, z = make_blobs(
-    centers=NUM_CLUSTERS,
+X, y = make_blobs(
+    centers=N_CLUSTERS,
     cluster_std=CLUSTER_STD,
+    n_features=10,
     n_samples=1000,
     random_state=12
 )
 
+clusterer = MyKMeans(data=X, n_clusters=N_CLUSTERS)
+cluster_labels, centroids, iters = clusterer.run()
 
-#k_means = KMeans(data=X, num_clusters=NUM_CLUSTERS)
-#hidden_values, centroids, iters = k_means.run()
-#
-#print('iters:', iters)
-#
+print('iters:', iters)
+
+
 #plt.scatter(X[:, 0], X[:, 1], c=hidden_values, s=10)
 #plt.scatter(centroids[:, 0], centroids[:, 1], c='black', s=200, marker='x')
 #plt.savefig(f"k_means_clusters.png")
 ##plt.show()
 
-expect_max = ExpectMax(data=X, num_clusters=NUM_CLUSTERS, cluster_std=CLUSTER_STD, max_iters=100)
-hidden_values, means, iters = expect_max.run()
-colors = np.array([np.argmax(v_row) for v_row in hidden_values])
-
-print('iters:', iters)
-
-plt.scatter(X[:, 0], X[:, 1], c=colors, s=10)
-plt.scatter(means[:, 0], means[:, 1], c='black', s=200, marker='x')
-#plt.savefig(f"expect_max_clusters.png")
-plt.show()
+#expect_max = ExpectMax(data=X, n_clusters=N_CLUSTERS, cluster_std=CLUSTER_STD, max_iters=100)
+#hidden_values, means, iters = expect_max.run()
+#colors = np.array([np.argmax(v_row) for v_row in hidden_values])
+#
+#print('iters:', iters)
+#
+#plt.scatter(X[:, 0], X[:, 1], c=colors, s=10)
+#plt.scatter(means[:, 0], means[:, 1], c='black', s=200, marker='x')
+##plt.savefig(f"expect_max_clusters.png")
+#plt.show()
