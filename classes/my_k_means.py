d116a16816d45764b19bc7cb4d2e591b1f07916a
Blake Wang <bwang@intouchhealth.com>
2020-03-10 09:05:33 -0700

    Handle the case where no points are in a cluster in MyKMeans

--- classes/my_k_means.py
+++ classes/my_k_means.py
@@ -45,4 +45,9 @@ class MyKMeans:
         self.prev_centers = np.copy(self.centers)
         for i in range(self.n_clusters):
             mask = np.array([label == i for label in self.cluster_labels])
-            self.centers[i] = np.mean(self.data[mask], axis=0)
+            if np.count_nonzero(mask) == 0:
+                # If there are no points in this cluster, then set the cluster
+                # center to be the origin.
+                self.centers[i] = np.zeros(self.data.shape[1])
+            else:
+                self.centers[i] = np.mean(self.data[mask], axis=0)
--- main.py
+++ main.py
@@ -3,25 +3,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets.samples_generator import make_blobs
+from sklearn.metrics import silhouette_score
 from classes.my_k_means import MyKMeans
 from classes.my_expect_max import MyExpectMax
 
-N_CLUSTERS = 5
-
 X, y = make_blobs(
-    centers=N_CLUSTERS,
-    n_features=3,
+    centers=3,
+    n_features=10,
     n_samples=1000,
     random_state=11
 )
 
-clusterers = [
-    MyKMeans(data=X, n_clusters=N_CLUSTERS),
-    MyExpectMax(data=X, n_clusters=N_CLUSTERS)
-]
+for n_clusters in range(4,5):
+    clusterers = [
+        MyKMeans(data=X, n_clusters=n_clusters),
+#        MyExpectMax(data=X, n_clusters=n_clusters)
+    ]
 
-for clusterer in clusterers:
-    cluster_labels, centers, iters = clusterer.run()
-    print('cluster_labels', np.unique(cluster_labels, return_counts=True))
-    print('centers', centers)
-    print('iters', iters)
+    for clusterer in clusterers:
+        print(n_clusters, clusterer.__class__.__name__)
+        cluster_labels, centers, iters = clusterer.run()
+#        score = silhouette_score(X, cluster_labels)
+#        print(f"  {score}")
+#        print('')
+
+#        print('cluster_labels', np.unique(cluster_labels, return_counts=True))
+#        print('centers', centers)
+#        print('iters', iters)
