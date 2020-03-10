d5ca630b8080755f73c66fb627775372dad638e0
Blake Wang <bwang@intouchhealth.com>
2020-03-10 10:04:24 -0700

    Save the printed metrics

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import time
+import pprint
 import matplotlib.pyplot as plt
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.metrics import silhouette_score
@@ -14,20 +16,46 @@ X, y = make_blobs(
     random_state=11
 )
 
+np.random.seed(11)
+random_states = np.random.choice(range(1000), size=5, replace=False)
+
+metrics = {
+    'MyKMeans': {'best_score': -1},
+    'MyExpectMax': {'best_score': -1}
+}
+
+pp = pprint.PrettyPrinter(indent=4)
+
 for n_clusters in range(3, 8):
-    clusterers = [
-        MyKMeans(data=X, n_clusters=n_clusters),
-        MyExpectMax(data=X, n_clusters=n_clusters)
-    ]
+    for random_state in random_states:
+        clusterers = [
+            MyKMeans(data=X, n_clusters=n_clusters, random_state=random_state),
+            MyExpectMax(data=X, n_clusters=n_clusters, random_state=random_state)
+        ]
 
-    for clusterer in clusterers:
-        cluster_labels, centers, iters = clusterer.run()
-        score = silhouette_score(X, cluster_labels)
+        for clusterer in clusterers:
+            start_time = time.time()
+            cluster_labels, centers, iters = clusterer.run()
+            elapsed = round(time.time() - start_time, 3)
+            score = silhouette_score(X, cluster_labels)
 
-        print(n_clusters, clusterer.__class__.__name__)
-        print(f"  {score}")
-        print('')
+            alg = clusterer.__class__.__name__
+            if score > metrics[alg]['best_score']:
+                metrics[alg]['best_score'] = score
+                metrics[alg]['n_clusters'] = n_clusters
+                metrics[alg]['random_state'] = random_state
+                metrics[alg]['iters'] = iters
+                metrics[alg]['elapsed'] = elapsed
 
-#        print('cluster_labels', np.unique(cluster_labels, return_counts=True))
-#        print('centers', centers)
-#        print('iters', iters)
+pp.pprint(metrics)
+
+#{   'MyExpectMax': {   'best_score': 0.8239206498003877,
+#                       'elapsed': 0.337,
+#                       'iters': 3,
+#                       'n_clusters': 5,
+#                       'random_state': 25},
+#    'MyKMeans': {   'best_score': 0.8239206498003877,
+#                    'elapsed': 0.175,
+#                    'iters': 3,
+#                    'n_clusters': 5,
+#                    'random_state': 25}}
