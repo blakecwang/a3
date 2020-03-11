f212ca0908fdfe5bc44ace7a5f78f1e08eb8f2df
Blake Wang <bwang@intouchhealth.com>
2020-03-11 07:00:14 -0700

    Plot blobs

--- main.py
+++ main.py
@@ -11,34 +11,17 @@ from sklearn.metrics import silhouette_score
 from classes.my_k_means import MyKMeans
 from classes.my_expect_max import MyExpectMax
 
-dataset_attrs = [('wine', 'quality'), ('breast_cancer', 'Diagnosis')]
-
-#for filename, target in dataset_attrs:
-#
-#    train = pd.read_csv(f'{filename}_train.csv')
-#    test = pd.read_csv(f'{filename}_test.csv')
-#
-#    y_train = train.loc[:,target]
-#    X_train = train.drop(target, axis=1)
-#    y_test = test.loc[:,target]
-#    X_test = test.drop(target, axis=1)
-
-X, y = make_blobs(
-    centers=3,
-    n_features=2,
-    n_samples=1000,
-    random_state=11
-)
-
-np.random.seed(11)
-random_states = np.random.choice(range(1000), size=5, replace=False)
-
-metrics = {
-    'MyKMeans': {'score': -1},
-    'MyExpectMax': {'score': -1}
-}
-
-pp = pprint.PrettyPrinter(indent=4)
+def plot_stuff(cluster_counts, k_means, expect_max, name):
+    font = { 'family': 'Arial', 'size': 18 }
+    plt.rc('font', **font)
+    plt.plot(cluster_counts, k_means, label='MyKMeans')
+    plt.plot(cluster_counts, expect_max, label='MyExpectMax')
+    plt.ylabel('Average Silhouette', fontsize=18, fontname='Arial')
+    plt.xlabel('Clusters', fontsize=18, fontname='Arial')
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig(f"{name}.png")
+    plt.show()
 
 def lowest_label_error(labels1, labels2):
     n_labels = np.unique(labels1).shape[0]
@@ -56,7 +39,38 @@ def lowest_label_error(labels1, labels2):
             lowest_error = error
     return lowest_error
 
-for n_clusters in range(3, 8):
+#dataset_attrs = [('wine', 'quality'), ('breast_cancer', 'Diagnosis')]
+#
+#for filename, target in dataset_attrs:
+#
+#    train = pd.read_csv(f'{filename}_train.csv')
+#    test = pd.read_csv(f'{filename}_test.csv')
+#
+#    y_train = train.loc[:,target]
+#    X_train = train.drop(target, axis=1)
+#    y_test = test.loc[:,target]
+#    X_test = test.drop(target, axis=1)
+
+X, y = make_blobs(
+    centers=6,
+    n_features=2,
+    n_samples=1000,
+    random_state=11
+)
+
+np.random.seed(11)
+random_states = np.random.choice(range(1000), size=5, replace=False)
+
+metrics = {
+    'MyKMeans': {'score': -1},
+    'MyExpectMax': {'score': -1}
+}
+
+k_means_scores = []
+expexct_max_scores = []
+cluster_counts = []
+for n_clusters in range(2, 11):
+    best_score = {'MyKMeans': -1, 'MyExpectMax': -1}
     for random_state in random_states:
         clusterers = [
             MyKMeans(data=X, n_clusters=n_clusters, random_state=random_state),
@@ -80,7 +94,19 @@ for n_clusters in range(3, 8):
                 metrics[alg]['elapsed'] = elapsed
                 metrics[alg]['error'] = lowest_label_error(cluster_labels, y)
 
-pp.pprint(metrics)
+            if score > best_score[alg]:
+                best_score[alg] = score
+
+    k_means_scores.append(best_score['MyKMeans'])
+    expexct_max_scores.append(best_score['MyExpectMax'])
+    cluster_counts.append(n_clusters)
+
+#print(k_means_scores)
+#print(expexct_max_scores)
+
+pprint.PrettyPrinter(indent=4).pprint(metrics)
+
+plot_stuff(cluster_counts, k_means_scores, expexct_max_scores, 'blobs')
 
 #{   'MyExpectMax': {   'elapsed': 0.228,
 #                       'iters': 3,
