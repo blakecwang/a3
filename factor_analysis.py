523291e8cf7551c9d7d7c94aee7c585a2b13f075
Blake Wang <bwang@intouchhealth.com>
2020-03-15 18:10:08 -0700

    Do Factor Analysis stuff

--- factor_analysis.py
+++ factor_analysis.py
@@ -2,29 +2,24 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.random_projection import GaussianRandomProjection
-from scipy.spatial import distance
+from sklearn.decomposition import FactorAnalysis
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 
 def plot_stuff(x_vals, y_vals, name):
     font = { 'family': 'Arial', 'size': 18 }
     plt.rc('font', **font)
-    plt.plot(x_vals, y_vals, marker='o')
-    plt.xlabel('New Dimension', fontsize=18, fontname='Arial')
-    plt.ylabel('Mean Distance Ratio', fontsize=18, fontname='Arial')
-    x_min = x_vals.min()
-    x_max = x_vals.max()
-    x_len = x_max - x_min
-    tol = 0.05
-    plt.xlim(x_max + tol * x_len, x_min - tol * x_len)
+    plt.plot(x_vals, y_vals)
+    plt.xlabel('Iterations', fontsize=18, fontname='Arial')
+    plt.ylabel('Log-Likelihood Score', fontsize=18, fontname='Arial')
     plt.tight_layout()
-    plt.savefig(f'{name}.png')
-#    plt.show()
+#    plt.savefig(f'{name}.png')
+    plt.show()
 
 RS = 11
 
-name = 'wq_random_project'
+name = 'wq_factor'
+tol = 50
 target = 'quality'
 train = pd.read_csv(f'wine_train.csv')
 test = pd.read_csv(f'wine_test.csv')
@@ -32,40 +27,19 @@ full = pd.concat([train, test])
 y = np.array(train.loc[:, target])
 X = np.array(train.drop(target, axis=1))
 
-name = 'gb_random_project'
-X, y = make_blobs(
-    centers=6,
-    n_features=2,
-    n_samples=1000,
-    random_state=11
-)
+#name = 'gb_factor'
+#tol = 5
+#X, y = make_blobs(
+#    centers=6,
+#    n_features=2,
+#    n_samples=1000,
+#    random_state=11
+#)
 
-n_pairs = 100
-np.random.seed(RS)
-sample_idxs = np.random.choice(range(X.shape[0]), size=2*n_pairs, replace=False)
-x_vals = np.array(range(1, X.shape[1] + 1))
-y_vals = []
-for n_components in x_vals:
-    print(X.shape[1], '->', n_components)
-    np.random.seed(RS)
-    best_mean = np.inf
-    for rs in np.random.choice(range(1000), size=5, replace=False):
-        transformer = GaussianRandomProjection(random_state=rs, n_components=n_components)
-        X_new = transformer.fit_transform(X)
+transformer = FactorAnalysis(n_components=7, random_state=RS, tol=tol)
+X_new = transformer.fit_transform(X)
 
-        ratios = []
-        for i in range(n_pairs):
-            idx1 = sample_idxs[2*i]
-            idx2 = sample_idxs[2*i+1]
-            d_old = distance.euclidean(X[idx1], X[idx2])
-            d_new = distance.euclidean(X_new[idx1], X_new[idx2])
-            ratio = d_new / d_old
-            ratios.append(ratio)
-        try_mean = np.mean(ratio)
-        if (try_mean - 1)**2 < best_mean:
-            best_mean = try_mean
-    y_vals.append(best_mean)
-    print('best_mean', best_mean)
-    print('')
+print(X.shape)
+print(X_new.shape)
 
-plot_stuff(x_vals, y_vals, name)
+plot_stuff(range(transformer.n_iter_), transformer.loglike_, name)
