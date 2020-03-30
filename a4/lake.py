d22b7f1a3d7915b958d7dcd1a982ae67337abf40
Blake Wang <bwang@intouchhealth.com>
2020-03-30 08:14:28 -0700

    Get confused

--- a4/lake.py
+++ a4/lake.py
@@ -12,29 +12,64 @@ try:
 except Exception:
     print('no args')
 
+DIM = 5 # must satisfy 4x + 1
+
+# +1 for frisbee
+# -1 for hole
+# 0 for frozen lake
+
+# Left: 0,
+# Down: 1,
+# Right: 2,
+# Up: 3
+
 LET = 'HFSG'
-dim = 5 # must satisfy 4x + 1
-np_map = np.zeros((dim, dim), dtype=int)
+np_map = np.zeros((DIM, DIM), dtype=int)
 toggle = True
-for i in range(dim):
+for i in range(DIM):
     if i % 2 == 0:
-        np_map[i] = np.ones(dim, dtype=int)
+        np_map[i] = np.ones(DIM, dtype=int)
     else:
-        idx = dim - 1 if toggle else 0
+        idx = DIM - 1 if toggle else 0
         np_map[i,idx] = 1
         toggle = not toggle
 np_map[0,0] = 2
-np_map[dim-1,dim-1] = 3
+np_map[DIM-1,DIM-1] = 3
 print(np_map)
 
 custom_map = []
-for i in range(dim):
+for i in range(DIM):
     mystr = ''
-    for j in range(dim):
+    for j in range(DIM):
         mystr += LET[np_map[i,j]]
     custom_map.append(mystr)
+
+#custom_map = [
+#    'SGHHH',
+#    'HHHHH',
+#    'HHHHH',
+#    'HHHHH',
+#    'HHHHH'
+#]
+
+custom_map = [
+    'SFFF',
+    'FHFH',
+    'FFFH',
+    'HFFG'
+]
+DIM=4
+
+#custom_map = [
+#    'FF',
+#    'GS'
+#]
+#DIM=2
+
+print('-' * DIM)
 for line in custom_map:
     print(line)
+print('-' * DIM)
 
 env = gym.make('FrozenLake-v0', desc=custom_map, is_slippery=True)
 #env = gym.make('FrozenLake8x8-v0', is_slippery=True)
@@ -58,18 +93,14 @@ print('R.shape', R.shape)
 D = 0.9
 
 vi = mdptoolbox.mdp.ValueIteration(P, R, D)
-if not stats:
-    # Display the variation of V.
-    vi.setVerbose()
+# vi.setVerbose()
 vi.run()
 
 if not stats:
     print('split')
 
 pi = mdptoolbox.mdp.PolicyIteration(P, R, D)
-if not stats:
-    # Display number of different actions between policy n-1 and n
-    pi.setVerbose()
+#pi.setVerbose()
 pi.run()
 
 if stats:
@@ -86,13 +117,13 @@ if stats:
     print('pi.iter', pi.iter)
     print('pi.time', pi.time)
     print('=======================')
-    print('same policy:', vi.policy == pi.policy)
+print('same policy:', vi.policy == pi.policy)
 
-DIR = 'ULDR'
-print(vi.policy.__class__)
-for i, d in enumerate(vi.policy):
-    if i % dim == 0:
-        print('')
-    else:
+#DIR = 'LDRU'
+DIR = 'v>^<' # verified using 2x2
+for policy in [vi.policy, pi.policy]:
+    for i, d in enumerate(policy):
+        if i % DIM == 0:
+            print('')
         print(DIR[d], end='')
-print('')
+    print('')
