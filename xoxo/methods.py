e0e50646df9102f661d8676a182de37c9e39d58c
Blake Wang <bwang@intouchhealth.com>
2020-04-10 12:11:51 -0700

    Get a bunch of stuff implemented

--- xoxo/methods.py
+++ xoxo/methods.py
@@ -12,9 +12,11 @@ def print_with_letters(board):
         letters = [LETTER_MAP[board[i,j]] for j in range(3)]
         print('|' + ' '.join(letters) + '|')
 
+# Return array of all combinations of 0, 1, 2 on a 3x3 grid.
 def all_boards():
     return np.array([[i[0:3],i[3:6],i[6:9]] for i in itertools.product(range(3), repeat=9)])
 
+# Return the number of i-streaks on the given board.
 def n_streaks(board, i):
     streak = np.full(3, i)
     n_streaks = 0
@@ -29,7 +31,24 @@ def n_streaks(board, i):
         n_streaks += 1
     return n_streaks
 
+# Return True if the differences between 1s and 2s is greater than 1.
 def is_imbalanced(board):
     o_count = np.count_nonzero(board == 1)
     x_count = np.count_nonzero(board == 2)
     return abs(o_count - x_count) > 1
+
+# Return the next board given the current board and a move.
+def next_board(board, move):
+    i = int(move / 3)
+    j = move % 3
+    new_board = np.copy(board)
+    if board[i,j] == 0:
+        board[i,j] == 2
+    return new_board
+
+# Return the index of board in boards.
+def find_index(board, boards):
+    i = 0
+    while not (boards[i] == board).all():
+        i += 1
+    return i
--- xoxo/ttt.py
+++ xoxo/ttt.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import methods as m
+import mdptoolbox, mdptoolbox.example
 
 # 0 -> ' '
 # 1 -> 'O'
@@ -16,7 +17,6 @@ import methods as m
 # It doesn't matter who goes first, a policy gives actions for all boards
 
 #m.print_with_letters(boards[1000])
-
 
 boards = []
 rewards = []
@@ -40,6 +40,31 @@ for board in m.all_boards():
 
 boards = np.array(boards)
 rewards = np.array(rewards)
-print(boards.shape)
-print(rewards.shape)
-print(boards)
+
+S = boards.shape[0]
+A = 9
+
+# P (A × S × S) -> (action x curr_state x next_state)
+# R (S × A)     -> (curr_state x action)
+P = np.zeros((A, S, S), dtype=np.bool_)
+R = np.zeros((S, A), dtype=np.int8)
+
+count = 0
+total = S * A
+for move in range(A):
+    for board_i, board in enumerate(boards):
+        next_board = m.next_board(board, move)
+        next_i = m.find_index(next_board, boards)
+        P[move,board_i,next_i] = 1
+        R[board_i,move] = rewards[next_i]
+
+        count += 1
+        print(count, '/', total)
+
+np.save('R.npy', R)
+np.save('P.npy', P)
+
+#R_load = np.load('R.npy')
+#P_load = np.load('P.npy')
+
+mdptoolbox.util.check(P, R)
